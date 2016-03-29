cd1bf255fc8c64c919d0ea8a7e6d44338db79851
Sean Williams <unixed@gmail.com>
2016-03-29 05:13:38 +0000

    Port Skewness(); still need Frequency()

--- entropy.go
+++ entropy.go
@@ -1,8 +1,16 @@
-package main
+package randumb
 
 import (
-  "fmt"
+  "sort"
 )
 
-func main() {
+func Skewness(data []byte, tuple int) float64 {
+  binMap := makeBinMap(data, tuple) 
+  values := []float64{}
+  for _, i := range binMap {
+    values = append(values, float64(i))
+  }
+
+  sort.Float64s(values)
+  return 3 * (avg(values) - median(values)) / stdDev(values)
 }
---
+++ util.go
@@ -0,0 +1,58 @@
+package randumb
+
+import (
+  "math"
+  "strconv"
+)
+
+func avg(nums []float64) float64 {
+  sum := 0.0
+  for i := range nums {
+    sum += nums[i]
+  }
+  return sum / float64(len(nums))
+}
+
+func stdDev(nums []float64) float64 {
+  anums := avg(nums)
+  return math.Sqrt(avg(mapFloat64(nums, func(f float64) float64 {
+    return math.Pow(f - anums, 2)
+  })))
+}
+
+func median(nums []float64) float64 {
+    var sub = 0
+    c := len(nums)
+    if c % 2 == 0 {
+      sub = 1
+    }
+    return nums[c/2-sub]
+}
+
+// Graciously plucked from gobyexample.com
+func mapFloat64(vs []float64, f func(float64) float64) []float64 {
+  vsm := make([]float64, len(vs))
+  for i, v := range vs {
+      vsm[i] = f(v)
+  }
+  return vsm
+} 
+
+// Map the 2^tuple number of keys to the frequency that bit sequence occurs.
+func makeBinMap(data []byte, tuple int) map[string]int {
+  binMap := map[string]int{}
+  binStr := ""
+
+  for _, d := range data {
+    for _, bit := range strconv.FormatInt(int64(d), 2) {
+      if len(binStr) == tuple {
+        binMap[binStr] += 1
+        binStr = ""
+      } else {
+        binStr += string(bit)
+      }
+    }
+  }
+
+  return binMap
+}
